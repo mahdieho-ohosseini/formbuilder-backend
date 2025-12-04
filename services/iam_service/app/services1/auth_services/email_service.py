fb1241db1e38140ab3ce57a86207c81c6680ab44
mahdieho-ohosseini <Mahdieh.hosseini1378@gmail.com>
2025-12-04 09:54:14 +0330

    create user_service

--- services/iam_service/app/services1/auth_services/email_service.py
+++ services/iam_service/app/services1/auth_services/email_service.py
@@ -1,11 +1,14 @@
 import smtplib
 from email.mime.text import MIMEText
 from services.iam_service.app.services1.base_service import BaseService
+from app.core.config import get_settings
 
 
 class EmailService(BaseService):
     def __init__(self):
         super().__init__()
+        self.settings = get_settings()
+
 
     def send_email(self, to_email: str, subject: str, body: str):
         msg = MIMEText(body)
